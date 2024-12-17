950500ba63cb56d2a8357238069522bce9ff3ed2
JonMikel <jon.perez05@somo.eus>
2024-12-17 11:49:22 +0100

    Añadidos getter y setter

--- src/clasesDato/Empleado.java
+++ src/clasesDato/Empleado.java
@@ -52,5 +52,48 @@ public class Empleado {
 		this.nombre = nombre;
 		this.apellido = apellido;
 	}
+	/// GETTER Y SETTER ///////
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public boolean isCarnet() {
+		return carnet;
+	}
+	public void setCarnet(boolean carnet) {
+		this.carnet = carnet;
+	}
+	public float getSueldo() {
+		return sueldo;
+	}
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
 }
 
